996d0e7ea38bfa4fdd8a83ea090fac5007553df7
Mustapha <mustapha.aouadi2@gmail.com>
2024-06-11 23:57:32 +0100

    addteees

---
+++ src/test/java/tn/esprit/Test.java
@@ -0,0 +1,11 @@
+package tn.esprit;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
